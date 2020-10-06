c6e99599fe7da35e9d9395e3ecfd0bb545c0fc74
Mae-Berces <eaajmmb@students.eaaa.dk>
2020-10-06 19:52:20 +0200

    update på api

--- js/js.js
+++ js/js.js
@@ -8,12 +8,12 @@ console.log('file: js/js.js loaded');
 $(document).ready(function () { // kører så snart DOM er klar
 
 
-    // ... codeing from here ...
-    
- mapboxgl.accessToken = 'pk.eyJ1IjoiamVubnltYWUiLCJhIjoiY2tmcWtlbGNtMGJ0NTJ3bzB2ODM2NWl3biJ9.OoNBfHTXEtINPw6pee5DVg';
-        var map = new mapboxgl.Map({
-            container: 'map', // container id
-            style: 'mapbox://styles/jennymae/ckfqkpnd20abv19t8ylbtazhm', // style URL
-            center: [10.218, 56.154], // starting position [lng, lat]
-            zoom: 14 // starting zoom
-        });
+            // ... codeing from here ...
+
+            mapboxgl.accessToken = 'pk.eyJ1IjoiamVubnltYWUiLCJhIjoiY2tmcWtlbGNtMGJ0NTJ3bzB2ODM2NWl3biJ9.OoNBfHTXEtINPw6pee5DVg';
+            var map = new mapboxgl.Map({
+                container: 'map', // container id
+                style: 'mapbox://styles/jennymae/ckfxt6os90k6p19qipk7uwlyf', // style URL
+                center: [10.218, 56.154], // starting position [lng, lat]
+                zoom: 14 // starting zoom
+            });
